00f3d576db84712ae9faee05f32147b4706db494
Nyall Dawson <nyall.dawson@gmail.com>
2017-10-01 18:47:09 +1000

    Explore is an action, add Goto action
    When dwarves see an attractive item, they goto get it

--- mine.py
+++ mine.py
@@ -358,13 +358,143 @@ class GoToAction(Action):
         if self.creature.x == self.x and self.creature.y == self.y:
             self.creature.remove_action(self)
 
+        new_x = self.creature.x
+        new_y = self.creature.y
+        if self.creature.x < self.x:
+            new_x += 1
+        elif self.creature.x > self.x:
+            new_x -= 1
+
+        def try_to_move_to(x, y):
+            if not self.creature.mine.is_valid_location(x, y):
+                return False
+            elif not self.creature.can_move(x, y):
+                return False
+            elif self.creature.mine.is_empty(x, y):
+                self.creature.move_to(x, y)
+                return True
+            elif self.creature.decided_to_dig(x, y):
+                self.creature.move_to(x, y)
+                self.creature.mine.set_material(x, y, Space())
+                return True
+            else:
+                return False
+
+        if not new_x == self.creature.x:
+            if try_to_move_to(new_x, new_y):
+                return
+            else:
+                new_x = self.creature.x
+
+        if self.creature.y < self.y:
+            new_y += 1
+        elif self.creature.y > self.y:
+            new_y -= 1
+
+        if not new_y == self.creature.y:
+            if try_to_move_to(new_x, new_y):
+                return
+
+        return
+
 
 class ExploreAction(Action):
-
     def __init__(self):
         Action.__init__(self)
+        self.x_dir = 0
+        self.xx_dir = 1
+        self.y_dir = 1
+        self.yy_dir = 1
+        self.frustration = 0
 
+    def think_about_changing_vertical_direction(self):
+        if self.creature.y == 0:
+            self.yy_dir = 1
+            self.y_dir = 1
+            return
+        if self.creature.y == self.creature.mine.height - 1:
+            self.yy_dir = -1
+            self.y_dir = -1
+            return
 
+        if random.randint(1, 1000) < self.frustration:
+            self.yy_dir *= -1
+            self.y_dir += self.yy_dir
+            self.y_dir = max(min(self.y_dir, 1), -1)
+            self.frustration *= 0.5
+            return
+
+        if random.randint(1, 1000) == 1:
+            self.y_dir += self.yy_dir
+            self.y_dir = max(min(self.y_dir, 1), -1)
+
+        # 'general' direction trend is hard to change
+        if random.randint(1, 10000) == 1:
+            self.yy_dir *= -1
+
+    def think_about_changing_horizontal_direction(self):
+        if self.creature.x == 0:
+            self.xx_dir = 1
+            self.x_dir = 1
+            return
+        if self.creature.x == self.creature.mine.width - 1:
+            self.xx_dir = -1
+            self.x_dir = -1
+            return
+
+        if random.randint(1, 1000) < self.frustration:
+            self.xx_dir *= -1
+            self.x_dir += self.xx_dir
+            self.x_dir = max(min(self.x_dir, 1), -1)
+            self.frustration *= 0.5
+            return
+
+        if random.randint(1, 1000) == 1:
+            self.x_dir += self.xx_dir
+            self.x_dir = max(min(self.x_dir, 1), -1)
+
+        # 'general' direction trend is hard to change
+        if random.randint(1, 10000) == 1:
+            self.xx_dir *= -1
+
+    def do(self):
+        new_x = self.creature.x
+        new_y = self.creature.y
+
+        self.think_about_changing_vertical_direction()
+        self.think_about_changing_horizontal_direction()
+
+        changed_level = False
+        if not changed_level and random.randint(1, 2) == 1:
+            new_x += self.x_dir
+            changed_level = True
+
+        if not changed_level and random.randint(1, 2) == 1:
+            new_y += self.y_dir
+
+        if new_x == self.creature.x and new_y == self.creature.y:
+            return
+
+        if not self.creature.mine.is_valid_location(new_x, new_y):
+            self.frustration += 1
+            return
+
+        if not self.creature.can_move(new_x, new_y):
+            self.frustration += 1
+            return
+
+        if self.creature.mine.is_empty(new_x, new_y):
+            self.creature.move_to(new_x, new_y)
+            self.frustration = 0
+
+        else:
+            # reluctant to dig
+            if self.creature.decided_to_dig(new_x, new_y):
+                self.creature.move_to(new_x, new_y)
+                self.creature.mine.set_material(new_x, new_y, Space())
+                self.frustration = 0
+            else:
+                self.frustration += 1
 
 
 class Creature:
@@ -378,9 +508,11 @@ class Creature:
         self.enchantments = []
         self.view_distance = 0
         self.actions = []
+        self.items = []
 
     def place_in_mine(self, mine):
         self.mine = mine
+        self.look()
 
     def push_action(self, action):
         self.actions.insert(0, action)
@@ -388,6 +520,9 @@ class Creature:
 
     def remove_action(self, action):
         self.actions.remove(action)
+
+    def add_item(self, item):
+        self.items.append(item)
 
     def die(self):
         self.mine.remove_creature(self)
@@ -421,7 +556,7 @@ class Creature:
         for c in visible_cells:
             if self.x == c[0] and self.y == c[1]:
                 continue
-            self.look_at(c[0],c[1])
+            self.look_at(c[0], c[1])
 
 
 class Wizard(Creature):
@@ -446,14 +581,14 @@ class Wizard(Creature):
 class Miner(Creature):
     def __init__(self, x, y):
         Creature.__init__(self, x, y)
-        self.x_dir = 0
-        self.xx_dir = 1
-        self.y_dir = 1
-        self.yy_dir = 1
-        self.frustration = 0
         self.likes_to_go_vertical = random.randint(10, 20)
         self.likes_to_go_horizontal = random.randint(10, 20)
         self.view_distance = 5
+        self.push_action(ExploreAction())
+
+    def place_in_mine(self, mine):
+        Creature.place_in_mine(self, mine)
+        mine.set_visibility(self.x, self.y, True)
 
     def can_move(self, x, y):
         for m in self.mine.creatures:
@@ -462,56 +597,6 @@ class Miner(Creature):
             if m.x == x and m.y == y:
                 return False
         return True
-
-    def think_about_changing_vertical_direction(self):
-        if self.y == 0:
-            self.yy_dir = 1
-            self.y_dir = 1
-            return
-        if self.y == self.mine.height - 1:
-            self.yy_dir = -1
-            self.y_dir = -1
-            return
-
-        if random.randint(1, 1000) < self.frustration:
-            self.yy_dir *= -1
-            self.y_dir += self.yy_dir
-            self.y_dir = max(min(self.y_dir, 1), -1)
-            self.frustration *= 0.5
-            return
-
-        if random.randint(1, 1000) == 1:
-            self.y_dir += self.yy_dir
-            self.y_dir = max(min(self.y_dir, 1), -1)
-
-        # 'general' direction trend is hard to change
-        if random.randint(1, 10000) == 1:
-            self.yy_dir *= -1
-
-    def think_about_changing_horizontal_direction(self):
-        if self.x == 0:
-            self.xx_dir = 1
-            self.x_dir = 1
-            return
-        if self.x == self.mine.width - 1:
-            self.xx_dir = -1
-            self.x_dir = -1
-            return
-
-        if random.randint(1, 1000) < self.frustration:
-            self.xx_dir *= -1
-            self.x_dir += self.xx_dir
-            self.x_dir = max(min(self.x_dir, 1), -1)
-            self.frustration *= 0.5
-            return
-
-        if random.randint(1, 1000) == 1:
-            self.x_dir += self.xx_dir
-            self.x_dir = max(min(self.x_dir, 1), -1)
-
-        # 'general' direction trend is hard to change
-        if random.randint(1, 10000) == 1:
-            self.xx_dir *= -1
 
     def decided_to_dig(self, x, y):
         if self.has_enchantment(Tricked):
