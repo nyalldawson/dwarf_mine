3d81d78c68e47284f95d97ef75686fb257e2e038
Nyall Dawson <nyall.dawson@gmail.com>
2019-03-21 17:58:53 +1000

    Add missing files

---
+++ book_of_spells.py
@@ -0,0 +1,31 @@
+import random
+from item import Item
+from creatures import Miner
+from enchantments import SleepSpell, Frozen, Firestarter
+
+
+class BookOfSpells(Item):
+    def  __init__(self, x, y):
+        Item.__init__(self, x, y)
+        self.char = 'B'
+        self.color = 948
+        self.type = 'Book of Spells'
+        spells = [SleepSpell, Frozen, Firestarter]
+        self.spell = spells[random.randint(0, len(spells) - 1)]()
+
+    def add_to_mine(self, mine):
+        Item.add_to_mine(self, mine)
+        mine.set_visibility(self.x, self.y, True)
+        hole_size = random.randint(2, 4)
+        mine.create_cave(self.x, self.y, hole_size)
+
+    def is_attractive_to(self, creature):
+        if isinstance(creature, Miner):
+            return True
+
+        return False
+
+    def found_by(self, creature):
+        if self.spell is not None:
+            creature.learn(self.spell)
+        creature.mine.push_message('{} found a {} and learnt the {} spell!'.format(creature.type, self.type, self.spell.type))
---
+++ item.py
@@ -0,0 +1,21 @@
+
+class Item:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.char = 'X'
+        self.color = 1
+        self.mine = None
+        self.type = 'thing'
+
+    def add_to_mine(self, mine):
+        self.mine = mine
+
+    def is_attractive_to(self, creature):
+        return False
+
+    def found_by(self, creature):
+        pass
+
+    def alter_color(self):
+        return None
