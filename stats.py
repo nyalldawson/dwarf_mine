a4006dfb0f6ccdfb45815be5ebfb3edb3e517e30
Nyall Dawson <nyall.dawson@gmail.com>
2021-06-07 07:48:01 +1000

    Better info

--- stats.py
+++ stats.py
@@ -22,15 +22,50 @@ class Stats():
         self.deaths.append((creature.type, death))
 
     def show(self):
-        self.show_treasures()
-        self.mine.print_current_level()
-        self.show_deaths()
+        shown_something = False
+        if self.show_treasures():
+            shown_something = True
+            self.mine.print_current_level()
 
-    def show_treasures(self):
+        if self.show_treasures():
+            shown_something = True
+            self.mine.print_current_level()
+
+        if self.show_items():
+            shown_something = True
+            self.mine.print_current_level()
+
+        if self.show_knowledge():
+            shown_something = True
+            self.mine.print_current_level()
+
+        if self.show_deaths():
+            shown_something = True
+            self.mine.print_current_level()
+
+        if not shown_something:
+            MessageBox(self.screen, 'Our story has just begun...')
+
+    def show_teams(self):
+        message = ['Teams','', '']
+        for tribe in self.mine.tribes:
+            message.append(f'{tribe.name} team')
+
+        MessageBox(self.screen,'\n'.join(message))
+
+    def show_treasures(self) -> bool:
         message = ['Treasures found','', '']
-        for tribe, treasures in self.treasures_collected.items():
+        found = False
+        for tribe in self.mine.tribes:
+            treasures = []
+            for c in self.mine.creatures:
+                if c.tribe == tribe:
+                    treasures.extend(i.type for i in c.items if isinstance(i, Treasure))
+
             if not treasures:
                 continue
+
+            found = True
 
             message.append(f'{tribe.name} team ({len(treasures)}):')
             c = Counter(treasures)
@@ -41,17 +76,77 @@ class Stats():
                     message.append('- {} {}s'.format(count, t))
             message.append('')
 
-        MessageBox(self.screen,'\n'.join(message))
+        if found:
+            MessageBox(self.screen,'\n'.join(message))
+        return found
 
-    def show_deaths(self):
+    def show_items(self) -> bool:
+        message = ['Items found','', '']
+        found = False
+        for tribe in self.mine.tribes:
+            treasures = []
+            for c in self.mine.creatures:
+                if c.tribe == tribe:
+                    treasures.extend(i.type for i in c.items if not isinstance(i, Treasure))
+
+            if not treasures:
+                continue
+
+            found = True
+            message.append(f'{tribe.name} team ({len(treasures)}):')
+            c = Counter(treasures)
+            for t, count in c.items():
+                if count == 1:
+                    message.append('- A {}'.format(t))
+                else:
+                    message.append('- {} {}s'.format(count, t))
+            message.append('')
+
+        if found:
+            MessageBox(self.screen,'\n'.join(message))
+
+        return found
+
+    def show_knowledge(self) -> bool:
+        message = ['Things learnt','', '']
+        found = False
+        for tribe in self.mine.tribes:
+            knowledge = []
+            for c in self.mine.creatures:
+                if c.tribe == tribe:
+                    knowledge.extend(i.type for i in c.knowledge)
+
+            if not knowledge:
+                continue
+
+            found = True
+
+            message.append(f'{tribe.name} team ({len(knowledge)}):')
+            c = Counter(knowledge)
+            for t, count in c.items():
+                if count == 1:
+                    message.append('- {}'.format(t))
+                else:
+                    message.append('- {} {}s'.format(count, t))
+            message.append('')
+
+        if found:
+            MessageBox(self.screen,'\n'.join(message))
+        return found
+
+    def show_deaths(self) -> bool:
         message = ['Creatures died','', '']
         c = Counter(self.deaths)
+        found = False
         for t, count in c.items():
+            found = True
             if count == 1:
                 message.append('- A {} {}'.format(t[0], t[1]))
             else:
                 message.append('- {} {}s {}'.format(count, t[0], t[1].replace('was','were')))
-        MessageBox(self.screen,'\n'.join(message))
+        if found:
+            MessageBox(self.screen,'\n'.join(message))
+        return found
 
 
 
